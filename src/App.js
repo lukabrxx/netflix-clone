b7b520a06bb3ebdc10c6fd42bff3d12fcf223f76
Luka Brkovic <lucabrkovic@gmx.de>
2023-01-23 17:19:23 +0100

    Login/reg backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
+    
     <BrowserRouter>
+    <AuthContextProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
